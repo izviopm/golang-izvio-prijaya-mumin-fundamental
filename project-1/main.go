c57f3aa7b3b728263c9565b5846a3469d6ae4808
iprijaya-mattel <Izvio.PrijayaMumin@Mattel.com>
2023-06-06 10:07:11 +0700

    Menambahkan fitur ubah data berdasarkan ID Buku

--- project-1/main.go
+++ project-1/main.go
@@ -11,7 +11,12 @@ import (
 )
 
 func main() {
-	books := [][]string{}
+	books := [][]string{
+		{"F001", "Bumi", "Tere Liye", "Gramedia", "Fantasy"},
+		{"F002", "Bulan", "Tere Liye", "Gramedia", "Fantasy"},
+		{"F003", "Matahari", "Tere Liye", "Gramedia", "Fantasy"},
+		{"F003", "Nama salah", "Tere Liye", "Gramedia", "Fantasy"},
+	}
 
 	fmt.Println("==================== Console Books Apps ====================")
 	fmt.Println("(1/Show All)      Tampilkan Semua Data")
@@ -62,6 +67,65 @@ func main() {
 			books = append(books, newData)
 
 			fmt.Println("Data Buku baru berhasil ditambahkan.")
+		} else if command == "filter data" || command == "3" {
+			fmt.Println("Filter Data Buku")
+			fmt.Println("Masukan ID Buku:")
+			scanner.Scan()
+			filter := strings.ToUpper(scanner.Text())
+
+			found := false
+			for _, v := range books {
+				if v[0] == filter {
+					table := tablewriter.NewWriter(os.Stdout)
+					table.SetHeader([]string{"ID", "Judul", "Pengarang", "Penerbit", "Genre"})
+					table.Append(v)
+					table.Render()
+					found = true
+				}
+			}
+
+			if !found {
+				fmt.Println("Data tidak ditemukan.")
+			}
+		} else if command == "update data" || command == "4" {
+			fmt.Println("Masukkan ID Buku yang ingin diubah:")
+			scanner.Scan()
+			filter := strings.ToUpper(scanner.Text())
+
+			foundIndex := -1
+			for i, v := range books {
+				if v[0] == filter {
+					foundIndex = i
+				}
+			}
+
+			if foundIndex != -1 {
+				bookID := filter
+
+				fmt.Println("Buku berhasil ditemukan dan akan diubah")
+				fmt.Println("Masukan Judul Buku yang benar:")
+				scanner.Scan()
+				bookTitle := scanner.Text()
+
+				fmt.Println("Masukan Nama Pengarang yang benar:")
+				scanner.Scan()
+				bookAuthor := scanner.Text()
+
+				fmt.Println("Masukan Nama Penerbit yang benar:")
+				scanner.Scan()
+				bookPublisher := scanner.Text()
+
+				fmt.Println("Masukan Genre Buku yang benar:")
+				scanner.Scan()
+				bookGenre := scanner.Text()
+
+				newData := []string{bookID, bookTitle, bookAuthor, bookPublisher, bookGenre}
+				books[foundIndex] = newData
+
+				fmt.Println("Data buku berhasil diubah.")
+			} else {
+				fmt.Println("Data buku tidak ditemukan.")
+			}
 		} else if command == "exit" || command == "6" {
 			fmt.Println("Program selesai.")
 			break
